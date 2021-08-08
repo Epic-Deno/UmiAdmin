81bb08af24bbcec73566d1f633dd5979c9967104
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-08-08 23:31:21 +0800

    初始化组件

--- src/pages/userManagement/index.tsx
+++ src/pages/userManagement/index.tsx
@@ -3,16 +3,120 @@
  * @Author: Pony
  * @Date: 2021-08-08 22:33:11
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-08 22:37:23
+ * @LastEditTime: 2021-08-08 23:30:39
  */
+import { Table } from 'antd';
 
 const userManagement = () => {
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: 100,
+      fixed: 'left',
+      filters: [
+        {
+          text: 'Joe',
+          value: 'Joe',
+        },
+        {
+          text: 'John',
+          value: 'John',
+        },
+      ],
+      onFilter: (value, record) => record.name.indexOf(value) === 0,
+    },
+    {
+      title: 'Other',
+      children: [
+        {
+          title: 'Age',
+          dataIndex: 'age',
+          key: 'age',
+          width: 150,
+          sorter: (a, b) => a.age - b.age,
+        },
+        {
+          title: 'Address',
+          children: [
+            {
+              title: 'Street',
+              dataIndex: 'street',
+              key: 'street',
+              width: 150,
+            },
+            {
+              title: 'Block',
+              children: [
+                {
+                  title: 'Building',
+                  dataIndex: 'building',
+                  key: 'building',
+                  width: 100,
+                },
+                {
+                  title: 'Door No.',
+                  dataIndex: 'number',
+                  key: 'number',
+                  width: 100,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: 'Company',
+      children: [
+        {
+          title: 'Company Address',
+          dataIndex: 'companyAddress',
+          key: 'companyAddress',
+          width: 200,
+        },
+        {
+          title: 'Company Name',
+          dataIndex: 'companyName',
+          key: 'companyName',
+        },
+      ],
+    },
+    {
+      title: 'Gender',
+      dataIndex: 'gender',
+      key: 'gender',
+      width: 80,
+      fixed: 'right',
+    },
+  ];
 
-    return (
-        <>
-            <h2>大家好这个用户管理页面</h2>
-        </>
-    )
-}
+  const data = [];
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      key: i,
+      name: 'John Brown',
+      age: i + 1,
+      street: 'Lake Park',
+      building: 'C',
+      number: 2035,
+      companyAddress: 'Lake Street 42',
+      companyName: 'SoftLake Co',
+      gender: 'M',
+    });
+  }
+  return (
+    <>
+      <Table
+        columns={columns}
+        dataSource={data}
+        bordered
+        size="middle"
+        scroll={{ x: 'calc(700px + 50%)', y: 240 }}
+      />
+    </>
+  );
+};
 
-export default userManagement
+export default userManagement;
--- src/pages/userManagement/userPermission.tsx
+++ src/pages/userManagement/userPermission.tsx
@@ -3,13 +3,22 @@
  * @Author: Pony
  * @Date: 2021-08-08 23:14:28
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-08 23:15:59
+ * @LastEditTime: 2021-08-08 23:28:15
  */
+import { Descriptions } from 'antd';
 
 export default () => {
-    return (
-        <>
-            <h3>用户权限</h3>
-        </>
-    )
-}
+  return (
+    <>
+      <Descriptions title="User Info" layout="vertical">
+        <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+        <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+        <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
+        <Descriptions.Item label="Address" span={2}>
+          No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+        </Descriptions.Item>
+        <Descriptions.Item label="Remark">empty</Descriptions.Item>
+      </Descriptions>
+    </>
+  );
+};
