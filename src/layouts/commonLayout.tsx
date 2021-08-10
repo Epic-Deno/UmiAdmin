e7f8a8ed0df74cfa3b105d3f32eb4063f70ab219
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-08-10 23:59:43 +0800

    优化模版404情况

---
+++ src/layouts/commonLayout.tsx
@@ -0,0 +1,9 @@
+/*
+ * @Description: 通用模板
+ * @Date: 2021-08-10 23:52:38
+ * @LastEditors: Pony
+ * @LastEditTime: 2021-08-10 23:53:06
+ */
+export default function (props: any) {
+  return <>{props.children}</>;
+}
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,12 +3,17 @@
  * @Author: Pony
  * @Date: 2021-08-08 23:17:13
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-10 22:22:31
+ * @LastEditTime: 2021-08-10 23:58:52
  */
 import { Redirect } from 'umi';
 import BaseLayout from './baseLayout';
+import CommonLayout from './commonLayout';
 
 export default (props: any) => {
+    const { location } = props
     // return <div style={{ padding: 20 }}>{ props.children }</div>;
+    if (!location.state) {
+        return <CommonLayout>{props.children}</CommonLayout>
+    }
     return <BaseLayout>{props.children}</BaseLayout>;
 }
