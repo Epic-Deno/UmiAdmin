7d707b1c2eae99e57ecb4418fcf908a1fb11dc25
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-08-11 10:23:02 +0800

    修改

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,9 @@
 /*
- * @Description: 主模板
+ * @Description: 主模板入口
  * @Author: Pony
  * @Date: 2021-08-08 23:17:13
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-10 23:58:52
+ * @LastEditTime: 2021-08-11 10:22:21
  */
 import { Redirect } from 'umi';
 import BaseLayout from './baseLayout';
@@ -11,7 +11,6 @@ import CommonLayout from './commonLayout';
 
 export default (props: any) => {
     const { location } = props
-    // return <div style={{ padding: 20 }}>{ props.children }</div>;
     if (!location.state) {
         return <CommonLayout>{props.children}</CommonLayout>
     }
