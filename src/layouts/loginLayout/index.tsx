dcbeda125f3b7c8b50f16dd8986a5439a33edfc3
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-08-12 22:15:57 +0800

    添加登录模板

--- src/layouts/loginLayout/index.tsx
+++ src/layouts/loginLayout/index.tsx
@@ -1,7 +1,12 @@
 /*
- * @Description: Created By Pony
+ * @Description: 登录的模板
  * @Author: Pony
  * @Date: 2021-08-12 22:11:44
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-12 22:11:45
+ * @LastEditTime: 2021-08-12 22:14:28
  */
+export default (props: any) => {
+    return (
+        
+    )
+}
