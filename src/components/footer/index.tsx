96e88d2f0d43442ff8dfdb68df203a2ff25ddf22
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-08-12 23:13:56 +0800

    脚部搭建

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,22 @@
+/*
+ * @Description: 网页脚部
+ * @Author: Pony
+ * @Date: 2021-08-12 23:10:44
+ * @LastEditors: Pony
+ * @LastEditTime: 2021-08-12 23:12:51
+ */
+import './index.less'
+export default () => (
+    <div className="footer">
+        <div className="copyright">
+          Copyright &copy;
+          <a href="https://github.com/Epic-Deno" target="_blank">
+            Github: Epic-Deno
+          </a>
+          版权所有
+        </div>
+        <div className="links">
+          <span>Epic-Deno</span>
+        </div>
+      </div>
+)
--- src/layouts/loginLayout/index.tsx
+++ src/layouts/loginLayout/index.tsx
@@ -3,16 +3,16 @@
  * @Author: Pony
  * @Date: 2021-08-12 22:11:44
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-12 23:07:36
+ * @LastEditTime: 2021-08-12 23:13:16
  */
+import Footer from '@/components/footer';
 import './index.css';
 
 export default (props: any) => {
-    return (
-        <div className="userLayout">
-            <div className="loginContainer">
-                {props.children}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="userLayout">
+      <div className="loginContainer">{props.children}</div>
+      <Footer/>
+    </div>
+  );
+};
