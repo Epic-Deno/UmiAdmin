00f90337aa9c10e6eb172ff96ad538bf06ca53cf
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-08-13 23:02:34 +0800

    登录页面布局调整

--- src/components/loginModule/index.tsx
+++ src/components/loginModule/index.tsx
@@ -3,9 +3,9 @@
  * @Author: Pony
  * @Date: 2021-08-12 23:22:49
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-13 22:35:13
+ * @LastEditTime: 2021-08-13 23:02:01
  */
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Row, Col, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import './index.less';
 export default () => {
@@ -17,6 +17,16 @@ export default () => {
         </Form.Item>
         <Form.Item>
           <Input.Password size="large" placeholder="Password: 123456" prefix={<LockOutlined />}/>
+        </Form.Item>
+        <Form.Item>
+          <Row justify="space-between" align="middle">
+            <Col span={8}>
+                <Checkbox style={{ marginRight: '6px' }}/>记住密码
+            </Col>
+            <Col span={6} style={{ textAlign: 'right' }}>
+                <a href="">忘记密码</a>
+            </Col>
+          </Row>
         </Form.Item>
         <Form.Item>
           <Button type="primary" block size="large" htmlType="submit">
