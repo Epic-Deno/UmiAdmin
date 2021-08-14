9da2fef1746bceec5c3e644998a6b55b025e199e
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-08-14 23:50:36 +0800

    布局调整

--- src/pages/dashboard/components/NumberCard.tsx
+++ src/pages/dashboard/components/NumberCard.tsx
@@ -3,7 +3,7 @@
  * @Author: Pony
  * @Date: 2021-08-14 22:45:16
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-14 23:25:58
+ * @LastEditTime: 2021-08-14 23:36:35
  */
 import React, { FC } from 'react';
 import { Card } from 'antd';
@@ -14,10 +14,10 @@ interface NumberCardType {
   icon: string;
   color: string;
   title: string;
-  numbers: number;
+  number: number;
 }
 
-const NumberCard: FC<NumberCardType> = ({ icon, color, title, numbers }) => {
+const NumberCard: FC<NumberCardType> = ({ icon, color, title, number }) => {
   return (
     <Card
       className={styles.numberCard}
@@ -30,7 +30,7 @@ const NumberCard: FC<NumberCardType> = ({ icon, color, title, numbers }) => {
       <div className={styles.content}>
         <p className={styles.title}>{title || 'No Title'}</p>
         <p className={styles.number}>
-          1222
+          {number && number.toLocaleString()}
         </p>
       </div>
     </Card>
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,7 @@
  * @Author: ZHANG ZHEN
  * @Date: 2021-07-02 23:04:10
  * @LastEditors: Pony
- * @LastEditTime: 2021-08-14 23:28:41
+ * @LastEditTime: 2021-08-14 23:31:24
  */
 import React, { useState } from 'react';
 import {  Row, Col, Card, Avatar, Skeleton } from 'antd';
@@ -19,21 +19,25 @@ function IndexPage() {
   const [numbers] = useState([
     {
       icon: 'pay-circle-o',
+      color: 'rgb(100, 234, 145)',
       title: 'Online Review',
       number: 2781
     },
     {
       icon: 'team',
+      color: 'rgb(143, 201, 251)',
       title: 'New Customers',
       number: 3241
     },
     {
       icon: 'message',
+      color: 'rgb(216, 151, 235)',
       title: 'Active Projects',
       number: 253
     },
     {
       icon: 'shopping-cart',
+      color: 'rgb(246, 152, 153)',
       title: 'Referrals',
       number: 4326
     },
